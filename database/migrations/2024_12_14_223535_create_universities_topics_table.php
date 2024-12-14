5644611c4a059dc205a8ba6c63518a6568dc4cff
Yunus Kırca <ynskrca52@outlook.com>
2024-12-15 01:41:29 +0300

    create universities_topics and cities_topics table

---
+++ database/migrations/2024_12_14_223535_create_universities_topics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('universities_topics', function (Blueprint $table) {
+            $table->id();
+            $table->integer('created_by');
+            $table->integer('user_id');
+            $table->integer('university_id');
+            $table->string('category');
+            $table->string('topic_title');
+            $table->text('topic_title_slug');
+            $table->text('comment');
+            $table->integer('likes')->default(0);
+            $table->integer('dislikes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('universities_topics');
+    }
+};
