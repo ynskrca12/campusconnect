2e1fc6662ef1c74d0032a31f1ab68de83dbd42dd
Yunus Kırca <ynskrca52@outlook.com>
2025-02-16 02:31:03 +0300

    added university logos to the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,23 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <div class="text-center mt-1  p-3" >
-                 
+            <div class="col-12 text-center">
+                <h3>Üniversiteler</h3>
+            </div>
+            <div class="col-12">
+                <div class="university-slider">
+                    <div class="logos">
+                        @foreach (File::glob(public_path('university logos') . '/*.{jpg,jpeg,png,gif}', GLOB_BRACE) as $image)
+                            <div class="logo-item">
+                                <img src="{{ asset('university logos/' . basename($image)) }}" class="img-fluid" alt="Üniversite Logosu">
+                            </div>
+                        @endforeach
+                        @foreach (File::glob(public_path('university logos') . '/*.{jpg,jpeg,png,gif}', GLOB_BRACE) as $image)
+                            <div class="logo-item">
+                                <img src="{{ asset('university logos/' . basename($image)) }}" class="img-fluid" alt="Üniversite Logosu">
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,12 +34,55 @@
 
 @section('css')
 <style>  
-    .container-scroller, .page-body-wrapper, .content-wrapper {
-        background-image: url('{{ asset('public/home_img/background.jpg') }}');
+    /* .container-scroller, .page-body-wrapper, .content-wrapper {
+        background-image: url('{{ asset('home_img/background.jpg') }}');
         background-position: center;
         background-size: cover;
-    }
+    } */
 
 </style>
+
+<style>
+    .university-slider {
+        overflow: hidden;
+        position: relative;
+        width: 100%;
+        background: rgba(255, 255, 255, 0.8);
+        padding: 10px;
+        border-radius: 10px;
+    }
+    
+    .logos {
+        display: flex;
+        width: max-content;
+        animation: slide 20s linear infinite;
+    }
+    
+    .logo-item {
+        flex: 0 0 auto;
+        width: 120px;
+        margin-right: 20px;
+    }
+    
+    .logo-item img {
+        width: 100%;
+        max-height: 100px;
+        object-fit: contain;
+        border-radius: 10px;
+    }
+
+    @keyframes slide {
+        from { transform: translateX(0); }
+        to { transform: translateX(-50%); } 
+    }
+</style>
 @endsection
+
+@section('js')
+<script>
+    $(document).ready(function () {
+        var copy = $(".logos").clone();
+        $(".university-slider").append(copy);
+    });
+</script>
 
