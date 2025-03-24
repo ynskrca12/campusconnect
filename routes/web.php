79f9450707525e87b34d634a9ae5a76ec63ca50e
Yunus Kırca <ynskrca52@outlook.com>
2025-03-24 10:37:39 +0300

    implement authentication and guest middleware for route protection

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,6 @@ use App\Http\Controllers\PageController;
 
 
 Route::get('/testmail',[AuthController::class, 'test'])->name('testmail');
-Route::get('/register',[AuthController::class, 'register'])->name('register');
-Route::post('/registerPost',[AuthController::class, 'registerPost'])->name('registerPost');
-Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
-Route::post('/reset-password-mail', [AuthController::class, 'sendResetMail']);
-
-Route::get('/reset-password', [AuthController::class, 'showResetForm'])->name('password.reset');
-Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
-
-
-Route::get('/login',[AuthController::class, 'login'])->name('login');
- Route::post('/login',[AuthController::class, 'loginPost'])->name('loginPost');
 
 Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
 
@@ -32,6 +21,61 @@ Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login',[AuthController::class, 'login'])->name('login');
+    Route::post('/login',[AuthController::class, 'loginPost'])->name('loginPost');
+
+    Route::get('/register',[AuthController::class, 'register'])->name('register');
+    Route::post('/registerPost',[AuthController::class, 'registerPost'])->name('registerPost');
+
+    Route::get('/reset-password', [AuthController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
+
+    Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
+    Route::post('/reset-password-mail', [AuthController::class, 'sendResetMail']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/universite/{id}/yorumlar',[StoreCommentController::class,'universite_yorum_ekle'])->name('universite_yorum_ekle');
+    Route::delete('/universite/{id}/yorumlar/{comment}',[DeleteCommentController::class,'universite_yorum_sil'])->name('universite_yorum_sil');
+
+    Route::get('kullanici_bilgileri',[UserController::class, 'kullanici_bilgileri'])->name('kullanici_bilgileri');
+    Route::get('kullanici_bilgileri_duzenle/{id}',[UserController::class, 'kullanici_bilgileri_duzenle'])->name('kullanici_bilgileri_duzenle');
+    Route::post('kullanici_bilgileri_duzenle_post',[UserController::class,'kullanici_bilgileri_duzenle_post'])->name('kullanici_bilgileri_duzenle_post');
+
+    Route::post('/update-profile', [UserController::class, 'updateProfile'])->name('user.updateProfile');
+    Route::get('/check-username', [UserController::class, 'checkUsername'])->name('user.checkUsername');
+    Route::get('/profile/check-email', [UserController::class, 'checkEmail'])->name('profile.check.email');
+    Route::post('/user/profile/update-email', [UserController::class, 'updateEmail'])->name('user.profile.update.email');
+    Route::get('/verify-email/{token}', [UserController::class, 'verifyEmail'])->name('user.verify.email');
+
+    Route::post('/city-topic/add', [CityController::class, 'addCityTopic']);
+    Route::post('/add/city/topic/comment', [CityController::class, 'storeComment'])->name('add.city.topic.comment');
+
+    //like disllike for city topic
+    Route::post('/city/topic/{id}/like', [CityController::class, 'like'])->name('topics.like');
+    Route::post('/city/topic/{id}/dislike', [CityController::class, 'dislike'])->name('topics.dislike');
+
+    Route::post('/university-topic/add', [UniversityController::class, 'addUniversityTopic']);
+    Route::post('/add/university/topic/comment', [UniversityController::class, 'storeComment'])->name('add.university.topic.comment');
+
+    //like dislikes for university topic
+    Route::post('/university/topic/{id}/like', [UniversityController::class, 'like'])->name('topics.like');
+    Route::post('/university/topic/{id}/dislike', [UniversityController::class, 'dislike'])->name('topics.dislike');
+
+    //create new topic in general form page
+    Route::post('/crete-new-topic-general-forum',[ForumController::class,'createTopicGeneralForum'])->name('create.topic.general.forum');
+
+    Route::post('/add/general/topic/comment', [ForumController::class, 'storeComment'])->name('add.general.topic.comment');
+
+    //like dislikes
+    Route::post('/general/topic/{id}/like', [ForumController::class, 'like'])->name('topics.like');
+    Route::post('/general/topic/{id}/dislike', [ForumController::class, 'dislike'])->name('topics.dislike');
+
+
+
+});
 
 
 Route::get('/universiteler',[UniversityController::class, 'index'])->name('universities');
@@ -42,67 +86,25 @@ Route::get('/cities/fetch', [CityController::class, 'fetchCities'])->name('unive
 
 Route::get('/universite_detay/{id}', [UniversityController::class, 'universite_detay'])->name('universite_detay');
 
-Route::get('/duyurular',[DuyuruController::class,'index']);
-
-Route::get('/ilanlar',[IlanController::class,'index'])->name('ilanlar');
-Route::get('ilan_ekle',[IlanController::class,'ilan_ekle'])->name('ilan_ekle');
-Route::post('ilan_ekle_post',[IlanController::class,'ilan_ekle_post'])->name('ilan_ekle_post');
-
 Route::get('/forum',[ForumController::class,'index'])->name('forum');
-
-Route::post('/universite/{id}/yorumlar',[StoreCommentController::class,'universite_yorum_ekle'])->name('universite_yorum_ekle');
-Route::delete('/universite/{id}/yorumlar/{comment}',[DeleteCommentController::class,'universite_yorum_sil'])->name('universite_yorum_sil');
 
 Route::get('devlet_universite_getir',[UniversityController::class,'devlet_universite_getir'])->name('devlet_universite_getir');
 Route::get('vakif_universite_getir',[UniversityController::class,'vakif_universite_getir'])->name('vakif_universite_getir');
-
-
-Route::get('kullanici_bilgileri',[UserController::class, 'kullanici_bilgileri'])->name('kullanici_bilgileri');
-Route::get('kullanici_bilgileri_duzenle/{id}',[UserController::class, 'kullanici_bilgileri_duzenle'])->name('kullanici_bilgileri_duzenle');
-Route::post('kullanici_bilgileri_duzenle_post',[UserController::class,'kullanici_bilgileri_duzenle_post'])->name('kullanici_bilgileri_duzenle_post');
-
-Route::post('/update-profile', [UserController::class, 'updateProfile'])->name('user.updateProfile');
-Route::get('/check-username', [UserController::class, 'checkUsername'])->name('user.checkUsername');
-Route::get('/profile/check-email', [UserController::class, 'checkEmail'])->name('profile.check.email');
-Route::post('/user/profile/update-email', [UserController::class, 'updateEmail'])->name('user.profile.update.email');
-Route::get('/verify-email/{token}', [UserController::class, 'verifyEmail'])->name('user.verify.email');
 
 Route::get('/forum/sehir/{slug}', [CityController::class, 'show'])->name('city.show');
 Route::get('/forum/sehir/mevzu/{slug}',[CityController::class,'topicComments'])->name('city.topic.comments');
 Route::get('/get-city-category-topics',[CityController::class,'getCityCategoryTopics']);
 Route::get('/get-city-category-topic-content',[CityController::class,'getCityCategoryTopicContent']);
-Route::post('/city-topic/add', [CityController::class, 'addCityTopic']);
-Route::post('/add/city/topic/comment', [CityController::class, 'storeComment'])->name('add.city.topic.comment');
-
-//like disllike for city topic
-Route::post('/city/topic/{id}/like', [CityController::class, 'like'])->name('topics.like');
-Route::post('/city/topic/{id}/dislike', [CityController::class, 'dislike'])->name('topics.dislike');
-
 
 Route::get('/forum/universite/{slug}', [UniversityController::class, 'show'])->name('university.show');
 Route::get('/forum/universite/mevzu/{slug}',[UniversityController::class,'topicComments'])->name('university.topic.comments');
 Route::get('/get-univercity-category-topics',[UniversityController::class,'getUnivercityCategoryTopics']);
 Route::get('/get-univercity-category-topic-content',[UniversityController::class,'getUnivercityCategoryTopicContent']);
-Route::post('/university-topic/add', [UniversityController::class, 'addUniversityTopic']);
-Route::post('/add/university/topic/comment', [UniversityController::class, 'storeComment'])->name('add.university.topic.comment');
-
-//like dislikes for university topic
-Route::post('/university/topic/{id}/like', [UniversityController::class, 'like'])->name('topics.like');
-Route::post('/university/topic/{id}/dislike', [UniversityController::class, 'dislike'])->name('topics.dislike');
-
-//create new topic in general form page
-Route::post('/crete-new-topic-general-forum',[ForumController::class,'createTopicGeneralForum'])->name('create.topic.general.forum');
 
 //random topics
 Route::get('/topics/random', [ForumController::class, 'getRandomTopics'])->name('topics.random');
 
 Route::get('/forum/mevzu/{slug}',[ForumController::class,'topicComments'])->name('topic.comments');
-Route::post('/add/general/topic/comment', [ForumController::class, 'storeComment'])->name('add.general.topic.comment');
-
-//like dislikes
-Route::post('/general/topic/{id}/like', [ForumController::class, 'like'])->name('topics.like');
-Route::post('/general/topic/{id}/dislike', [ForumController::class, 'dislike'])->name('topics.dislike');
-
 
 Route::fallback(function () {
     return view('errors.404');
